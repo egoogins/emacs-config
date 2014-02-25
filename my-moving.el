45a05d2fe8ad4606e690f044527b10e5ade7f98a
EJ Googins <egoogins@mitre.org>
2014-02-25 17:37:48 -0500

    Cycle through buffers with ctrl tab

--- my-moving.el
+++ my-moving.el
@@ -5,6 +5,10 @@
 ;; Set C-c, g to goto-line
 (global-set-key "\C-cg" 'goto-line)
 
+;; Scroll through buffers with Ctrl + Tab
+(global-set-key [C-tab] 'previous-buffer)
+(global-set-key [C-S-iso-lefttab] 'next-buffer)
+
 ;; Scroll faster
 (setq lazy-lock-defer-on-scrolling t)
 (setq lazy-lock-continuity-time 0.3)
